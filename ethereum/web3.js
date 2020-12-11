91322ee442195a61e58c6bac3cda2fa686f9268d
= <max@medicalkush.org>
2020-12-10 20:20:45 -0800

    add ethereum directory / sub-directories and web3 connection

---
+++ ethereum/web3.js
@@ -0,0 +1,18 @@
+import Web3 from "web3";
+
+let web3;
+
+if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
+  // in browser and metamask is running
+  web3 = new Web3(window.ethereum);
+  window.ethereum.enable();
+  window.ethereum.on("accountsChanged", function(accounts) {
+    window.location.reload(false);
+  });
+} else {
+  // on the server or user is not running metamask
+  const provider = new Web3.providers.HttpProvider(process.env.INFURA_API);
+  web3 = new Web3(provider);
+}
+
+export default web3;
