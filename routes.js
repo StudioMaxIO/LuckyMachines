4c24137ef1a40a48144ba4da9d58e26c9df35aca
= <max@medicalkush.org>
2021-01-19 21:15:28 -0800

    redirect if not owner on operator page, display no info for invalid game ID

--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@ routes
   .add("/play/:address/g/:gameID", "/play")
   .add("/play/:address", "/play")
   .add("/play", "/load")
+  .add("/operate/:address/invalid", "/invalid-operator")
   .add("/operate/:address", "/operate")
   .add("/operate", "/load-operator")
   .add("/new", "/factory");
