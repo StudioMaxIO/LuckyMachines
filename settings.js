1748bc771e5f0a1ce403b98f9a9b8cb06956aff6
= <max@medicalkush.org>
2021-01-23 19:52:44 -0800

    new factory address

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const settings = {
-  FACTORY_ADDRESS: "0xbbAa4Ef89697c70E3568eeA54b26412d8e49635e"
+  FACTORY_ADDRESS: "0x55d7b9c25aB117D8De3c20447ce22C7e37b0a31e"
 };
 
 module.exports = settings;
