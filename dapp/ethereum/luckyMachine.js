4cb2d2e30d4c61ff364efe5d60f9475e8de502d9
= <max@medicalkush.org>
2021-02-27 10:20:27 -0800

    add names to functions

--- dapp/ethereum/luckyMachine.js
+++ dapp/ethereum/luckyMachine.js
@@ -1,6 +1,8 @@
 import web3 from "./web3";
 import LuckyMachine from "./build/LuckyMachine.json";
 
-export default address => {
+const LM = address => {
   return new web3.eth.Contract(LuckyMachine.abi, address);
 };
+
+export default LM;
--- dapp/ethereum/luckyMachineFactory.js
+++ dapp/ethereum/luckyMachineFactory.js
@@ -1,6 +1,8 @@
 import web3 from "./web3";
 import LuckyMachineFactory from "./build/LuckyMachineFactory.json";
 
-export default address => {
+const LMFactory = address => {
   return new web3.eth.Contract(LuckyMachineFactory.abi, address);
 };
+
+export default LMFactory;
