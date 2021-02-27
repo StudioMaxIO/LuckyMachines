d49ab8ad7be16082c4d1c7eff9dda4d0b5613e56
Max Meier <max@studiomax.io>
2021-02-27 14:34:14 -0800

    remove inactive community links

--- dapp/ethereum/luckyMachine.js
+++
@@ -1,8 +0,0 @@
-import web3 from "./web3";
-import LuckyMachine from "./build/LuckyMachine.json";
-
-const LM = address => {
-  return new web3.eth.Contract(LuckyMachine.abi, address);
-};
-
-export default LM;
--- dapp/ethereum/luckyMachineFactory.js
+++
@@ -1,8 +0,0 @@
-import web3 from "./web3";
-import LuckyMachineFactory from "./build/LuckyMachineFactory.json";
-
-const LMFactory = address => {
-  return new web3.eth.Contract(LuckyMachineFactory.abi, address);
-};
-
-export default LMFactory;
